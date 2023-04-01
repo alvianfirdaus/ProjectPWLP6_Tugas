41d4ac668095b292f1306f9b9ff3487db1e3c2bc
MuhammadEgaRam <2141720143@student.polinema.ac.id>
2023-04-01 13:20:25 +0700

    Tugas PWLP 6 Update Alvian

--- app/Http/Controllers/PengalamanController.php
+++ app/Http/Controllers/PengalamanController.php
@@ -9,7 +9,9 @@ class PengalamanController extends Controller
 {
     public function pengalaman()
     {
+        // Pengalaman Alvian
         $data1 = Pengalaman::where('id', 1)->get();
+        // Pengalaman Ega
         $data2 = Pengalaman::where('id', 2)->get();
         return view('mahasiswas.kuliah', compact('data1', 'data2'));
     }
--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,11 +9,13 @@ class ProfileController extends Controller
 {
     public function profile1()
     {
+        // Profile 1 Alvian
         $dataprofil = Profil::where('nama', 'alvian')->get();
         return view('mahasiswas.profile1', compact('dataprofil'));
     }
     public function profile2()
     {
+        // Profile 2 Ega
         $dataprofil = Profil::where('nama', 'ega')->get();
         return view('mahasiswas.profile2', compact('dataprofil'));
     }
